f2f044b07b91ca981ccb350e1b3d5bdca0e2ce85
Selskiy <2466776@cegep-heritage.qc.ca>
2026-09-02 12:31:39 -0400

    Add CRUD operations in ProductCategory model

--- psH60Store/psH60A01/Models/ProductCategory.cs
+++ psH60Store/psH60A01/Models/ProductCategory.cs
@@ -10,4 +10,35 @@ public partial class ProductCategory
     public string ProdCat { get; set; } = null!;
 
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+
+    public static List<ProductCategory> GetAllProductCategories(H60AssignmentDbPsContext context)
+    {
+        return context.ProductCategories.OrderBy(p => p.CategoryId).ToList();
+    }
+
+    
+    public static ProductCategory GetProductCategoryById(H60AssignmentDbPsContext context, int id)
+    {
+        return context.ProductCategories.FirstOrDefault(x => x.CategoryId == id);
+    }
+    
+
+    public void Create(H60AssignmentDbPsContext context, ProductCategory productCategory)
+    {
+        context.ProductCategories.Add(productCategory);
+        context.SaveChanges();
+    }
+
+    public void Update(H60AssignmentDbPsContext context, ProductCategory productCategory)
+    {
+        context.ProductCategories.Update(productCategory);
+        context.SaveChanges();
+    }
+
+    public void Delete(H60AssignmentDbPsContext context, int id)
+    {
+        var productCategory = context.ProductCategories.FirstOrDefault(x => x.CategoryId == id);
+        context.ProductCategories.Remove(productCategory);
+        context.SaveChanges();
+    }
 }
--- psH60Store/psH60A01/obj/Debug/net8.0/psH60A01.AssemblyInfo.cs
+++ psH60Store/psH60A01/obj/Debug/net8.0/psH60A01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("psH60A01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0884c2724791e38d36ce0f2686d843006243d152")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+324ce096c99690ad247421c11589c98557c79b8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("psH60A01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("psH60A01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
